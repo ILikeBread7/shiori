fe0b2ba9a61bdc12bea02b1f7e17620e98455be0
I_LIKE_BREAD7 <pjaworski47@gmail.com>
2023-11-21 18:34:50 +0100

    Remove gold from menu screen

---
+++ js/plugins/ILB7_RemoveGoldFromMainMenu.js
@@ -0,0 +1,18 @@
+//=============================================================================
+// ILB7_RemoveGoldFromMainMenu.js
+//=============================================================================
+
+/*:
+ * @plugindesc Removes gold from the main menu screen
+ * @author I_LIKE_BREAD7
+ *
+ * @help This plugin does not provide plugin commands.
+ */
+
+(function() {
+
+    Scene_Menu.prototype.createGoldWindow = function() {
+        // empty
+    };
+
+})();
